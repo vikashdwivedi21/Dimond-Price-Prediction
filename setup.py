a2afa218f5ef41b2fc486fb6a4e52e66950584c5
vikashdwivei21 <mrvikashdwivedi021@gmail.com>
2023-12-31 12:28:59 +0530

    file updated

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    """The function `get_requirements` reads a file and returns a list of requirements, removing any
+    occurrences of the "-e.".
+    
+    Param: file_path (str) -- The file path is a string that represents the path to the file containing the
+    requirements
+    
+    returns: A list of requirements.
+    """    
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+#setup - project informations, name, version etc
+# The `setup()` function is a function provided by the `setuptools` library in Python. It is used to
+# define the metadata and configuration options for a Python package.
+
+setup(
+    name= "Dimond Price Prediction",
+    version= "0.0.1",
+    author= "Vikash Dwivedi",
+    author_email= "mrvikashdwivedi021@gmail.com",
+    install_requires= get_requirements("requirements.txt"),
+    packages= find_packages()
+)
